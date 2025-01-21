aa286b68ad24694226c4f181c466c49c5f342dd7
Marco Tascini <tascinimarco@tiscali.it>
2025-01-21 20:53:03 +0100

    triangle_sierpinski

--- visualizer/triforce_transformation.py
+++ visualizer/triforce_transformation.py
@@ -2,10 +2,42 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 
-side_len = 2
+SIDE_LEN = 2
 left_corner = np.array([0,0])
-right_corner = left_corner + np.array([side_len,0])
-top_corner = left_corner+ np.array([side_len/2,(side_len*math.pow(3,1/3))/2])
+right_corner = left_corner + np.array([SIDE_LEN,0])
+top_corner = left_corner+ np.array([SIDE_LEN/2,(SIDE_LEN*math.pow(3,1/3))/2])
+MAX_REC_DEPTH = 6
+XLIM_NEG = -1
+XLIM_POS = 3
+YLIM_NEG = -1
+YLIM_POS = 3
+
+simple = False
+
+def triangle_sierpinski():
+    triangle_list = []
+    triangle_sierpinski_rec(left_corner,right_corner,top_corner,triangle_list,0)
+
+    for t in triangle_list:
+        plt.gca().add_patch(plt.Polygon(t,fill=False))
+ 
+    plt.xlim(XLIM_NEG, XLIM_POS)
+    plt.ylim(YLIM_NEG, YLIM_POS)
+    plt.show()    
+
+def triangle_sierpinski_rec(left_corner,right_corner,top_corner, triangle_list, rec_depth):
+    if rec_depth > MAX_REC_DEPTH:
+        return
+    triangle_list.append(np.array([left_corner,right_corner,top_corner]))
+
+    left_half = [(left_corner[0] + top_corner[0])/2,(left_corner[1] + top_corner[1])/2]
+    right_half = [(top_corner[0] + right_corner[0])/2,(top_corner[1] + right_corner[1])/2]
+    bottom_half = [(left_corner[0] + right_corner[0])/2,(left_corner[1] + right_corner[1])/2]
+    rec_depth = rec_depth + 1
+    triangle_sierpinski_rec(left_corner,bottom_half,left_half,triangle_list,rec_depth)
+    triangle_sierpinski_rec(left_half,right_half,top_corner,triangle_list,rec_depth)
+    triangle_sierpinski_rec(bottom_half,right_corner,right_half,triangle_list,rec_depth)
+    
 
 def simple_triforce():
 
@@ -15,18 +47,21 @@ def simple_triforce():
 
     X2= []
     for p in X1:
-        X2.append(p + np.array([side_len,0]))
+        X2.append(p + np.array([SIDE_LEN,0]))
     t2 = plt.Polygon(X2)
     plt.gca().add_patch(t2)
 
     X3= []
     for p in X1:
-        X3.append(p + np.array([side_len/2,(side_len*math.pow(3,1/3))/2]))
+        X3.append(p + np.array([SIDE_LEN/2,(SIDE_LEN*math.pow(3,1/3))/2]))
     t3 = plt.Polygon(X3)
     plt.gca().add_patch(t3)
 
-    plt.xlim(-10, 10)
-    plt.ylim(-10, 10)
+    plt.xlim(XLIM_NEG, XLIM_POS)
+    plt.ylim(YLIM_NEG, YLIM_POS)
     plt.show()
 
-simple_triforce()    
+if simple:
+    simple_triforce()  
+else:
+    triangle_sierpinski()  

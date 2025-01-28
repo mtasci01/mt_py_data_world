d63d345b7468ebe3700ea2c4a577270868da11a8
Marco Tascini <tascinimarco@tiscali.it>
2025-01-28 18:52:04 +0100

    harmonic step

--- visualizer/harmonic_series.py
+++ visualizer/harmonic_series.py
@@ -1,12 +1,14 @@
 from matplotlib import pyplot as plt
 
-N_ITER = 100000
+N_ITER = 1000000
+STEP_SIZE = 10000
 xs=[]
 ys=[]
 sum = 0
 for i in range(1,N_ITER):
-    xs.append(i)
-    ys.append(sum)
+    if (i % STEP_SIZE == 0): 
+        xs.append(i)
+        ys.append(sum)
     sum = sum + 1/i
 
 plt.scatter(xs,ys, color='red')
